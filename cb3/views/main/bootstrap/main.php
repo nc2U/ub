388d338811a4b5ba76289dbe1e373d0a901dad17
Austin Kho <kori.susie@gmail.com>
2024-08-22 00:23:21 +0900

    main view update

--- cb3/views/main/bootstrap/main.php
+++ cb3/views/main/bootstrap/main.php
@@ -1,5 +1,48 @@
 <?php
-echo '<div class="col-xs-12 mb20" style="padding: 0;"><img src="uploads/img/main.jpg" width="100%" style="border: 1px solid #ddd;" /></div>';
+echo '<div class="col-xs-12 mb20" style="padding: 0;">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="uploads/img/main.jpg" alt="Slide 1">
+      <div class="carousel-caption">
+        <!-- <p>Description for Slide 1.</p> -->
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="uploads/img/main.jpg" alt="Slide 2">
+      <div class="carousel-caption">
+       <!-- <p>Description for Slide 2.</p> -->
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="uploads/img/main.jpg" alt="Slide 3">
+      <div class="carousel-caption">
+        <!-- <p>Description for Slide 3.</p> -->
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>';
 
 $active = element('bl', $view) === '1';
 
