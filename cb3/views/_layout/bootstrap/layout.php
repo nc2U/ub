071d842e7c312eb7cc7ec5955a377c6cfcfff6ee
Austin Kho <kori.susie@gmail.com>
2024-08-21 19:00:11 +0900

    layout update

--- cb3/views/_layout/bootstrap/layout.php
+++ cb3/views/_layout/bootstrap/layout.php
@@ -18,32 +18,30 @@
 <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />
 <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo element('layout_skin_url', $layout); ?>/css/style.css" />
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/earlyaccess/nanumgothic.css" />
-<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/ui-lightness/jquery-ui.css" />
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/earlyaccess/nanumgothic.css" />
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/ui-lightness/jquery-ui.css" />
 <?php echo $this->managelayout->display_css(); ?>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <script type="text/javascript">
-// 자바스크립트에서 사용하는 전역변수 선언
-var cb_url = "<?php echo trim(site_url(), '/'); ?>";
-var cb_cookie_domain = "<?php echo config_item('cookie_domain'); ?>";
-var cb_charset = "<?php echo config_item('charset'); ?>";
-var cb_time_ymd = "<?php echo cdate('Y-m-d'); ?>";
-var cb_time_ymdhis = "<?php echo cdate('Y-m-d H:i:s'); ?>";
-var layout_skin_path = "<?php echo element('layout_skin_path', $layout); ?>";
-var view_skin_path = "<?php echo element('view_skin_path', $layout); ?>";
-var is_member = "<?php echo $this->member->is_member() ? '1' : ''; ?>";
-var is_admin = "<?php echo $this->member->is_admin(); ?>";
-var cb_admin_url = <?php echo $this->member->is_admin() === 'super' ? 'cb_url + "/' . config_item('uri_segment_admin') . '"' : '""'; ?>;
-var cb_board = "<?php echo isset($view) ? element('board_key', $view) : ''; ?>";
-var cb_board_url = <?php echo ( isset($view) && element('board_key', $view)) ? 'cb_url + "/' . config_item('uri_segment_board') . '/' . element('board_key', $view) . '"' : '""'; ?>;
-var cb_device_type = "<?php echo $this->cbconfig->get_device_type() === 'mobile' ? 'mobile' : 'desktop' ?>";
-var cb_csrf_hash = "<?php echo $this->security->get_csrf_hash(); ?>";
-var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
+    // 자바스크립트에서 사용하는 전역변수 선언
+    var cb_url = "<?php echo trim(site_url(), '/'); ?>";
+    var cb_cookie_domain = "<?php echo config_item('cookie_domain'); ?>";
+    var cb_charset = "<?php echo config_item('charset'); ?>";
+    var cb_time_ymd = "<?php echo cdate('Y-m-d'); ?>";
+    var cb_time_ymdhis = "<?php echo cdate('Y-m-d H:i:s'); ?>";
+    var layout_skin_path = "<?php echo element('layout_skin_path', $layout); ?>";
+    var view_skin_path = "<?php echo element('view_skin_path', $layout); ?>";
+    var is_member = "<?php echo $this->member->is_member() ? '1' : ''; ?>";
+    var is_admin = "<?php echo $this->member->is_admin(); ?>";
+    var cb_admin_url = <?php echo $this->member->is_admin() === 'super' ? 'cb_url + "/' . config_item('uri_segment_admin') . '"' : '""'; ?>;
+    var cb_board = "<?php echo isset($view) ? element('board_key', $view) : ''; ?>";
+    var cb_board_url = <?php echo ( isset($view) && element('board_key', $view)) ? 'cb_url + "/' . config_item('uri_segment_board') . '/' . element('board_key', $view) . '"' : '""'; ?>;
+    var cb_device_type = "<?php echo $this->cbconfig->get_device_type() === 'mobile' ? 'mobile' : 'desktop' ?>";
+    var cb_csrf_hash = "<?php echo $this->security->get_csrf_hash(); ?>";
+    var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
 </script>
 <!--[if lt IE 9]>
 <script type="text/javascript" src="<?php echo base_url('assets/js/html5shiv.min.js'); ?>"></script>
@@ -378,5 +376,9 @@ Layout URL : <?php echo element('layout_skin_url', $layout); ?>,
 Skin Directory : <?php echo element('view_skin_path', $layout); ?>,
 Skin URL : <?php echo element('view_skin_url', $layout); ?>,
 -->
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
