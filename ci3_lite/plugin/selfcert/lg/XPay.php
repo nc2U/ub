74060270237731e77555c11e0f61753629009277
Austin Kho <kori.susie@gmail.com>
2024-08-27 22:42:52 +0900

    remove lite version

--- ci3_lite/plugin/selfcert/lg/XPay.php
+++
@@ -1,9 +0,0 @@
-<?php
-        // mall.conf 설정 추가를 위한 XPayClient 확장
-        class XPay extends XPayClient
-        {
-            public function set_config_value($key, $val)
-            {
-                $this->config[$key] = $val;
-            }
-        }
--- ci3_lite/views/document/mobile/document.php
+++
@@ -1,3 +0,0 @@
-<h3><?php echo html_escape(element('doc_title', element('data', $view))); ?></h3>
-
-<?php echo element('content', element('data', $view)); ?>
